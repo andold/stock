148b4396c710e36dedad3bebbbc4ba6d9bf21e4f
andold <andold@naver.com>
2024-01-16 22:05:13 +0900

    스케쥴 추가

--- src/main/java/kr/andold/stock/ScheduledTasks.java
+++ src/main/java/kr/andold/stock/ScheduledTasks.java
@@ -67,7 +67,7 @@ public class ScheduledTasks {
 	}
 
 	// 평일
-	@Scheduled(cron = "50 40 22 * * MON-FRI")
+	@Scheduled(cron = "50 40 22 * * MON-SAT")
 	public void scheduleTaskDaily() {
 		log.info("{} scheduleTaskDaily()", Utility.indentStart());
 		long started = System.currentTimeMillis();
