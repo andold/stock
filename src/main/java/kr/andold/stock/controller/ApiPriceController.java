ead0f1d435f5dc60b567f9b563804f96e70ccdf1
andold <andold@naver.com>
2024-01-26 17:03:38 +0900

    OutOfMemoryError on download #32 - 파일로 만들고 스트림 전송

--- src/main/java/kr/andold/stock/controller/ApiPriceController.java
+++ src/main/java/kr/andold/stock/controller/ApiPriceController.java
@@ -1,12 +1,20 @@
 package kr.andold.stock.controller;
 
-import java.io.UnsupportedEncodingException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +23,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import jakarta.servlet.http.HttpServletResponse;
 import kr.andold.stock.domain.PriceDomain;
 import kr.andold.stock.param.ItemParam;
 import kr.andold.stock.param.PriceParam;
@@ -29,7 +37,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("api/price")
 public class ApiPriceController {
-	@Autowired private HttpServletResponse httpServletResponse;
 	@Autowired private PriceService service;
 
 	@PostMapping(value = {"search"})
@@ -66,16 +73,44 @@ public class ApiPriceController {
 
 	@ResponseBody
 	@GetMapping(value = {"download"})
-	public String download() throws UnsupportedEncodingException {
+	public ResponseEntity<StreamingResponseBody> download() throws IOException {
 		log.info("{} download()", Utility.indentStart());
+		long started = System.currentTimeMillis();
 
 		String filename = URLEncoder.encode(String.format("stock-price-%s.json", LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE)), "UTF-8").replaceAll("\\+", "%20");
-		httpServletResponse.setHeader("Content-Disposition", "attachment; filename=" + filename);
+		File file = File.createTempFile("stock-price-", "-.json.tmp");
+		file.deleteOnExit();
+		FileWriter fw = new FileWriter(file);
+		PrintWriter writer = new PrintWriter(fw);
+		List<PriceDomain> prices = service.search(null);
+		for (PriceDomain price : prices) {
+			writer.println(Utility.toStringJson(price));
+		}
+		writer.close();
+		
+		final HttpHeaders httpHeaders = new HttpHeaders();
+	    final InputStream inputStream = new FileInputStream(file);
+	    httpHeaders.set(HttpHeaders.LAST_MODIFIED, String.valueOf(file.lastModified()));
+	    httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"");
+	    httpHeaders.set(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()));
 
-		String response = service.download();
+	    StreamingResponseBody responseBody = outputStream -> {
 
-		log.info("{} {} - download()", Utility.indentEnd(), Utility.ellipsisEscape(response, 32, 32));
-		return response;
+	        int numberOfBytesToWrite;
+	        byte[] data = new byte[64 * 1024];
+	        while ((numberOfBytesToWrite = inputStream.read(data, 0, data.length)) != -1) {
+	            outputStream.write(data, 0, numberOfBytesToWrite);
+	        }
+
+	        inputStream.close();
+	    };
+
+		log.info("{} #{}/#{} - download() - {}", Utility.indentEnd(), Utility.size(prices), file.length(), Utility.toStringPastTimeReadable(started));
+	    return ResponseEntity.ok()
+	            .headers(httpHeaders)
+	            .contentLength(file.length())
+	            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+	            .body(responseBody);		
 	}
 
 	@PostMapping(value = "upload")
