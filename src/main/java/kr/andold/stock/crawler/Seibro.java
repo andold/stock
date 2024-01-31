29ed97809ccb980eddebdd53974ca7167d24d004
andold <andold@naver.com>
2024-01-31 20:45:38 +0900

    bug fix) 종목 상세 가져 오는데 exception

--- src/main/java/kr/andold/stock/crawler/Seibro.java
+++ src/main/java/kr/andold/stock/crawler/Seibro.java
@@ -1275,17 +1275,22 @@ public class Seibro implements Crawler {
 				driver.findElement(BY_SEARCH_CODE_RESULT, TIMEOUT).click();
 			} else {
 				List<WebElement> candidates = driver.findElements(BY_SEARCH_CODE_RESULT, TIMEOUT);
+				boolean found = false;
 				for (WebElement candidate : candidates) {
 					String href = candidate.getAttribute("href");	// javascript:SelectedValueReturn( 'KR7391680006', '흥국HK하이볼액티브증권상장지수투자신탁[주식]' ) 
 					if (href.matches(String.format(".+KR.%s.+", code))) {
 						candidate.click();
+						found = true;
 						break;
 					}
 				}
-				driver.quit();
-				Result<ParserResult> result = Result.<ParserResult>builder().status(STATUS.FAIL_NO_RESULT).build();
-				log.debug("{} 『{}』 itemCompany({}) - {}", Utility.indentEnd(), result, code, Utility.toStringPastTimeReadable(started));
-				return result;
+				
+				if (!found) {
+					driver.quit();
+					Result<ParserResult> result = Result.<ParserResult>builder().status(STATUS.FAIL_NO_RESULT).build();
+					log.debug("{} 『{}』 itemCompany({}) - {}", Utility.indentEnd(), result, code, Utility.toStringPastTimeReadable(started));
+					return result;
+				}
 			}
 
 			//	팝업이 닫혔다, 돌아간다
