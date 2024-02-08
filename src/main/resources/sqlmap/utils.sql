7e45a0b4964f1f7c1f41f198cc7adba39b78f2e0
andold <andold@naver.com>
2024-02-08 23:43:12 +0900

    상장폐지일 수집하면서 수집되는 것 지우기

--- src/main/resources/sqlmap/utils.sql
+++ src/main/resources/sqlmap/utils.sql
@@ -2,3 +2,7 @@
 DELETE from stock_item				WHERE code > 'KR' AND code < 'KS';
 DELETE from stock_dividend_history	WHERE code > 'KR' AND code < 'KS';
 DELETE from stock_price				WHERE code > 'KR' AND code < 'KS';
+
+-- 상장폐지일 수집하면서 수집되는 것 지우기
+DELETE from stock_item				WHERE ipo_open IS NULL AND ipo_close IS NOT NULL;
+
