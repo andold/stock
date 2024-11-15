c6f3caef3fd2a2dca8550868708dc13316557b82
andold <andold@naver.com>
2024-11-16 01:24:47 +0900

    resources-opi5

---
+++ src/main/resources-opi5/install-stock-opi5.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#
+PROFILE=opi5
+THIS_SCRIPT_FILE_NAME=install-stock-$PROFILE.sh
+DEPLOY_SCRIPT_FILE_NAME=deploy.sh
+HOME_DIR=/home/andold
+SOURCE_DIR=$HOME_DIR/src/github/stock
+DEPLOY_DIR=$HOME_DIR/deploy/stock
+TOMCAT_BIN_DIR=$HOME_DIR/apps/tomcat/bin
+#
+#
+git config --global core.quotepath false
+#
+#
+cd	$SOURCE_DIR
+git stash
+git pull
+git log --pretty=format:"%h - %an, %ai:%ar : %s" -8
+#
+#
+# copy deploy script file
+cp	$SOURCE_DIR/src/main/resources-$PROFILE/$DEPLOY_SCRIPT_FILE_NAME	$DEPLOY_DIR
+#
+#
+cd	$DEPLOY_DIR
+chmod	a+x $DEPLOY_SCRIPT_FILE_NAME
+bash $DEPLOY_SCRIPT_FILE_NAME
+#
