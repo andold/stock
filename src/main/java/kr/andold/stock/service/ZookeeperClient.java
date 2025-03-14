d8f5c5daa8eb4b010bf2383d43fa197dfaff707c
andold <andold@naver.com>
2025-03-14 12:49:21 +0900

    bug fix) there is no one master

--- src/main/java/kr/andold/stock/service/ZookeeperClient.java
+++ src/main/java/kr/andold/stock/service/ZookeeperClient.java
@@ -3,6 +3,8 @@ package kr.andold.stock.service;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+
+import org.apache.zookeeper.AddWatchMode;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
@@ -19,10 +21,12 @@ import org.apache.zookeeper.ZooKeeper;
 @Slf4j
 @Service
 public class ZookeeperClient implements Watcher {
+	private static final String ZNODE_PREFIX = "c_";
+
 	@Getter private static boolean isMaster = false;
+	@Getter private static String currentZNodeName = "";
 
 	private ZooKeeper zookeeper;
-	private String currentZNodeName;
 
 	@Getter private static String zookeeperConnectString;
 	@Value("${application.zookeeper.connect.string}")
@@ -42,18 +46,9 @@ public class ZookeeperClient implements Watcher {
 		log.info("{} run() - 『{}』『{}』", Utility.indentStart(), zookeeperConnectString, zookeeperZnodeElectPath);
 
 		try {
-			zookeeper = new ZooKeeper(zookeeperConnectString, 3000, this);
-//			zookeeper.create("/test/elect-stock", new byte[] {}, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-//			zookeeper.create("/elect-stock", new byte[] {}, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-			String zNodeFullPath = zookeeper.create(zookeeperZnodeElectPath + "/c_", new byte[] {}, Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-			currentZNodeName = zNodeFullPath.replace(zookeeperZnodeElectPath + "/", "");
-			log.info("{} run() - 『zNodeFullPath:{}』『currentZNodeName:{}』", Utility.indentMiddle(), zNodeFullPath, currentZNodeName);
+			zookeeper = new ZooKeeper(getZookeeperConnectString(), 3000, this);
 		} catch (IOException e) {
-			log.error("{} IOException:: {}", Utility.indentMiddle(), e.getMessage(), e);
-		} catch (KeeperException e) {
-			log.error("{} KeeperException:: {}", Utility.indentMiddle(), e.getMessage(), e);
-		} catch (InterruptedException e) {
-			log.error("{} InterruptedException:: {}", Utility.indentMiddle(), e.getMessage(), e);
+			log.error("IOException:: {}", e.getMessage(), e);
 		}
 
 		log.info("{} run() - 『{}』『{}』", Utility.indentEnd(), zookeeperConnectString, zookeeperZnodeElectPath);
@@ -61,45 +56,84 @@ public class ZookeeperClient implements Watcher {
 
 	@Override
 	public void process(WatchedEvent event) {
-		log.info("{} process(『{}』)", Utility.indentStart(), event);
+		log.info("{} process(『{}』) - 『{}:{}』『{}』", Utility.indentStart(), event, event.getType(), event.getState(), currentZNodeName);
 
 		switch(event.getType()) {
 		case None:
-			if (event.getState() == Event.KeeperState.SyncConnected) {
-				log.info("{} process(『{}』) - Successfully connected to zookeeper!!", Utility.indentMiddle(), event);
-			} else {
-				synchronized (zookeeper) {
-					log.info("{} process(『{}』) - Disconnected from zookeeper event", Utility.indentMiddle(), event);
-					zookeeper.notifyAll();
+			switch(event.getState()) {
+			case AuthFailed:
+				break;
+			case Closed:
+				try {
+					zookeeper = new ZooKeeper(getZookeeperConnectString(), 3000, this);
+				} catch (IOException e) {
+					log.error("IOException:: {}", e.getMessage(), e);
 				}
+				break;
+			case ConnectedReadOnly:
+				break;
+			case Disconnected:
+				updateMaster();
+				break;
+			case Expired:
+				try {
+					zookeeper.removeWatches(getZookeeperZnodeElectPath(), this, WatcherType.Persistent, true);
+					zookeeper.close();
+					currentZNodeName = "";
+				} catch (InterruptedException e) {
+					log.error("InterruptedException:: {}", e.getMessage(), e);
+				} catch (KeeperException e) {
+					log.error("KeeperException:: {}", e.getMessage(), e);
+				}
+				break;
+			case SaslAuthenticated:
+				break;
+			case SyncConnected:
+				try {
+					zookeeper.addWatch(getZookeeperZnodeElectPath(), this, AddWatchMode.PERSISTENT);
+					String zNodeFullPath = zookeeper.create(getZookeeperZnodeElectPath() + "/" + ZNODE_PREFIX, new byte[] {}, Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+					currentZNodeName = zNodeFullPath.replace(getZookeeperZnodeElectPath() + "/", "");
+				} catch (KeeperException e) {
+					log.error("KeeperException:: {}", e.getMessage(), e);
+				} catch (InterruptedException e) {
+					log.error("InterruptedException:: {}", e.getMessage(), e);
+				}
+				break;
+			default:
+				break;
 			}
+			break;
 		case NodeDeleted:
 		case NodeCreated:
 		case NodeDataChanged:
 		case NodeChildrenChanged:
-			try {
-				List<String> children = zookeeper.getChildren(zookeeperZnodeElectPath, false);
-				ZookeeperClient.isMaster = isMaster(children);
-			} catch(KeeperException e) {
-			} catch(InterruptedException e) {
-			}
+			updateMaster();
 			break;
 		default:
+			updateMaster();
 			break;
 		}
 		
-		log.info("{} process(『{}』)", Utility.indentEnd(), event);
+		log.info("{} process(『{}』) - 『{}:{}』『{}』", Utility.indentEnd(), event, event.getType(), event.getState(), currentZNodeName);
 	}
 
-	private boolean isMaster(List<String> children) {
-		log.info("{} isMaster({}) - {}", Utility.indentMiddle(), children, currentZNodeName);
-		if (children == null || children.isEmpty() || currentZNodeName == null) {
+	private boolean updateMaster() {
+		if (zookeeper == null) {
 			return false;
 		}
 
-		Collections.sort(children);
-		String smallestChild = children.get(0);
-		return smallestChild.equals(currentZNodeName);
+		try {
+			List<String> children = zookeeper.getChildren(getCurrentZNodeName(), false);
+			Collections.sort(children);
+			String smallestChild = children.get(0);
+			isMaster = smallestChild.equals(currentZNodeName);
+			log.info("{} updateMaster() - {} {} {}", Utility.indentMiddle(), children, currentZNodeName, isMaster);
+			return true;
+		} catch(Exception e) {
+			log.error("Exception:: {}", e.getMessage(), e);
+		}
+
+		return false;
 	}
 
 }
